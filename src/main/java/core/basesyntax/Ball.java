a89390030b6c28d577b93ec07e9f8298eb9dd2ef
AlexPranschke <alexpranschke@gmail.com>
2026-03-08 22:39:08 +0100

    fix2

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,18 +1,23 @@
 package core.basesyntax;
+
 public class Ball {
     private final String color;
     private final int number;
+
     public Ball(String color, int number) {
         this.color = color;
         this.number = number;
     }
+
     public String getColor() {
         return color;
     }
+
     public int getNumber() {
         return number;
     }
-     @Override
+
+    @Override
     public String toString() {
         return "Ball{" + "color='" + getColor() + '\'' + ", number=" + getNumber() + '}';
     }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,9 +6,15 @@ public class Lottery {
     private static final int MIN_SIZE = 1;
     private static final int MAX_SIZE = 100;
     private static final Random RANDOM = new Random();
-    ColorSupplier colorSupplier = new ColorSupplier();
+    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    public ColorSupplier getColorSupplier() {
+        return colorSupplier;
+    }
 
     public Ball getRandomBall() {
-        return new Ball(colorSupplier.getRandomColor(), RANDOM.nextInt(MAX_SIZE - MIN_SIZE + 1) + MIN_SIZE);
+        String color = colorSupplier.getRandomColor();
+        int size = RANDOM.nextInt(MAX_SIZE - MIN_SIZE + 1) + MIN_SIZE;
+        return new Ball(color, size);
     }
 }
