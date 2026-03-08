8784e11c2097fb494f6461fcc1ccff72f9c5b995
AlexPranschke <alexpranschke@gmail.com>
2026-03-08 22:28:02 +0100

    color supplier 1 isntannnce

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,12 +1,16 @@
 package core.basesyntax;
+
 import java.util.Random;
+
 public class Lottery {
     private static final int MIN_SIZE = 1;
     private static final int MAX_SIZE = 100;
+    private static final Random RANDOM = new Random();
+    ColorSupplier colorSupplier = new ColorSupplier();
+
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
         String randomColor = colorSupplier.getRandomColor();
-        int randomSize = (int) (Math.random() * (MAX_SIZE - MIN_SIZE + 1)) + MIN_SIZE;
+        int randomSize = RANDOM.nextInt(MAX_SIZE - MIN_SIZE + 1) + MIN_SIZE;
         return new Ball(randomColor, randomSize);
     }
 }
